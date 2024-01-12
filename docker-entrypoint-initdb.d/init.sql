722996108849615a71b799cfb3ee2bd000d49ffd
Jacob <Arthursjacob01@gmail.com>
2024-01-12 17:27:20 -0600

    refactor docker entrypoint sql

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -49,46 +49,77 @@ VALUES
 INSERT INTO expected_category_distribution (category_id, distribution, created_date, last_updated_date)
 VALUES
     (1, 25, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Housing
-    (2, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Transportation
-    (3, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Food
-    (4, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Utilities
+    (2, 15, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Transportation
+    (3, 15, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Food
+    (4, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Utilities
     (5, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Insurance
-    (6, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Medical & Healthcare
-    (7, 10, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),  -- Saving, Investing, & Debt Payments
+    (6, 5, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Medical & Healthcare
+    (7, 5, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Saving, Investing, & Debt Payments
     (8, 5, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Personal Spending
     (9, 5, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258'),   -- Recreation & Entertainment
     (10, 5, '2023-11-30 13:56:02.845258', '2023-11-30 13:56:02.845258');  -- Miscellaneous
 
---Function to generate dummy expense data (roughly 1000 rows) based on the expected category distribution rates
+-- Function to generate dummy expense data based on the expected category distribution rates with variations
 DO $$
 DECLARE
     distribution_record RECORD;
     category_id INT;
-    distribution INT;
+    min_distribution INT;
+    max_distribution INT;
     random_percentage INT;
     total_expenses INT := 1000;
     num_expenses INT;
     i INT;
     expense_date TIMESTAMP;
+    title_prefix VARCHAR(255);
+    description_prefix VARCHAR(255);
 BEGIN
-    FOR distribution_record IN (SELECT * FROM expected_category_distribution) LOOP
-        category_id := distribution_record.category_id;
-        distribution := distribution_record.distribution;
+    -- Start a transaction
+    BEGIN
+        FOR distribution_record IN (SELECT * FROM expected_category_distribution) LOOP
+            -- Extract values from the distribution_record
+            category_id := distribution_record.category_id;
+            min_distribution := distribution_record.distribution - 5;
+            max_distribution := distribution_record.distribution + 5;
 
-        -- Generate a random percentage within the specified distribution
-        random_percentage := floor(random() * distribution);
+            -- Generate a random percentage within the specified distribution
+            random_percentage := floor(random() * (max_distribution - min_distribution + 1) + min_distribution);
 
-        -- Calculate the number of expenses based on the percentage
-        num_expenses := CEIL((random_percentage / 100.0) * total_expenses);
+            -- Calculate the number of expenses based on the percentage
+            num_expenses := CEIL((random_percentage / 100.0) * total_expenses);
 
-        FOR i IN 1..num_expenses LOOP
-            -- Generate a random date between current date and 6 months ago
-            expense_date := CURRENT_DATE - (floor(random() * 180) || ' days')::INTERVAL;
+            -- Set prefix for title and description based on category
+            title_prefix := CASE
+                WHEN category_id = 1 THEN 'Housing Expense'
+                WHEN category_id = 2 THEN 'Transportation Cost'
+                WHEN category_id = 3 THEN 'Food Expense'
+                WHEN category_id = 4 THEN 'Utilities Expense'
+                WHEN category_id = 5 THEN 'Insurance Expense'
+                WHEN category_id = 6 THEN 'Medical & Healthcare Expense'
+                WHEN category_id = 7 THEN 'Saving, Investing, & Debt Payments Expense'
+                WHEN category_id = 8 THEN 'Personal Spending Expense'
+                WHEN category_id = 9 THEN 'Recreation & Entertainment Expense'
+                WHEN category_id = 10 THEN 'Miscellaneous Expense'
+                ELSE 'Generic Expense'
+            END;
+
+            description_prefix := 'Details for ' || title_prefix;
 
             -- Insert dummy expense data into the expense table
-            INSERT INTO expense (category_id, title, description, created_date, last_updated_date)
-            VALUES (category_id, 'Expense ' || i, 'Description for Expense ' || i, expense_date, expense_date);
+            FOR i IN 1..num_expenses LOOP
+                -- Generate a random date between current date and 6 months ago
+                expense_date := CURRENT_DATE - (floor(random() * 180) || ' days')::INTERVAL;
+
+                -- Insert dummy expense data
+                INSERT INTO expense (category_id, title, description, created_date, last_updated_date)
+                VALUES (category_id, title_prefix || ' ' || i, description_prefix || ' ' || i, expense_date, expense_date);
+            END LOOP;
         END LOOP;
-    END LOOP;
+    EXCEPTION
+        WHEN OTHERS THEN
+            RAISE NOTICE 'Error: %', SQLERRM;
+    END;
+
+    COMMIT;
 END
 $$;
