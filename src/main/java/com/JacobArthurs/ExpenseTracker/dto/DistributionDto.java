b8d78ba8edeea72cd1d0cc3aa5448718ae7e4a65
Jacob <Arthursjacob01@gmail.com>
2024-01-20 13:17:05 -0600

    Refactor DistributionDto

--- src/main/java/com/JacobArthurs/ExpenseTracker/dto/DistributionDto.java
+++ src/main/java/com/JacobArthurs/ExpenseTracker/dto/DistributionDto.java
@@ -1,16 +1,13 @@
 package com.JacobArthurs.ExpenseTracker.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class DistributionDto {
     private List<String> categories;
     private List<Integer> distributions;
-
-    public DistributionDto(List<String> categories, List<Integer> distributions) {
-        this.categories = categories;
-        this.distributions = distributions;
-    }
 }
